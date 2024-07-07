df24e0e40de57d8306a27ef4a8e9c5f21b075bcc
GuestAariyatech <aariyatechguest@gmail.com>
2024-07-07 15:03:19 +0530

    Fix padding of AllCoursesScreen

--- app/(home)/course/AllCoursesScreen.tsx
+++ app/(home)/course/AllCoursesScreen.tsx
@@ -55,9 +55,9 @@ export default AllCoursesScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
-        padding: 10,
+        padding: 5,
     },
     wrapper: {
         marginTop: 100,
