fed6218908feb1b65b4ddf7ffbb00b8ca1d184cd
GuestAariyatech <aariyatechguest@gmail.com>
2024-07-08 15:02:56 +0530

    new sub branch of feature/school-details

--- webSocket/socket.ts
+++ webSocket/socket.ts
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://ef3e-223-233-85-160.ngrok-free.app"); //	use the IP address of your machine
+export const socket = io("https://60bf-223-233-85-160.ngrok-free.app"); //	use the IP address of your machine
